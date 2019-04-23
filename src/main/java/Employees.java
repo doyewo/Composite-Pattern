c1b3fc8f375cbdd0da25c3750fed8223b2035ad5
doyewo <dexteroyewo@gmail.com>
2019-04-22 22:25:54 -0500

    Created classes

---
+++ src/main/java/Employees.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employees {
+    private List<Employees> aide;
+    private String dept;
+    private String name;
+    String int salary;
+
+    public Employees(String name, String dept, int sal) {
+
+    }
+}
