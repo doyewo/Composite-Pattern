823aa5d3c68ab9888087fb4fc79869aea3bca946
doyewo <dexteroyewo@gmail.com>
2019-04-22 22:43:08 -0500

    Employees class done

--- src/main/java/Employees.java
+++ src/main/java/Employees.java
@@ -7,7 +7,22 @@ public class Employees {
     private String name;
     String int salary;
 
-    public Employees(String name, String dept, int sal) {
+    public Employees(String name, String dept, int salary) {
+        this.salary = salary;
+        this.name = name;
+        this.dept = dept;
+        aide = new ArrayList<Employees>();
+    }
 
+    public void remove (Employees e) {
+        aide.remove(e);
+    }
+
+    public void add (Employees e) {
+        aide.add(e);
+    }
+
+    public List<Employees> getAide() {
+        return ("Employees: (Name: " + name + "Department: " + dept + "Salary: " + salary + ")");
     }
 }
